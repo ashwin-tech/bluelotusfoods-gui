6e4b88d15359f9210c12e6322d916840d62c86ad
ashwin-tech <ashwin.education1@example.com>
2025-10-25 01:47:39 -0500

    fixing build errors-1

--- vendor-apps/quote-form/src/common/FishDropDown.tsx
+++ vendor-apps/quote-form/src/common/FishDropDown.tsx
@@ -3,12 +3,16 @@ import GenericDropdown from "./GenericDropdown";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Use environment variable
 
+interface Fish {
+  common_name: string;
+}
+
 const FishDropdown: React.FC<{
   value: string;
   onChange: (val: string) => void;
 }> = ({ value, onChange }) => {
   return (
-    <GenericDropdown
+    <GenericDropdown<Fish>
       fetchUrl={`${API_BASE_URL}/fish/types`} // Use base URL
       value={value}
       onChange={onChange}
--- vendor-apps/quote-form/src/common/GradeDropdown.tsx
+++ vendor-apps/quote-form/src/common/GradeDropdown.tsx
@@ -3,12 +3,16 @@ import GenericDropdown from "./GenericDropdown";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Use environment variable
 
+interface Grade {
+  name: string;
+}
+
 const GradeDropdown: React.FC<{
   value: string;
   onChange: (val: string) => void;
 }> = ({ value, onChange }) => {
   return (
-    <GenericDropdown
+    <GenericDropdown<Grade>
       fetchUrl={`${API_BASE_URL}/fish/grade`} // Use base URL
       value={value}
       onChange={onChange}
